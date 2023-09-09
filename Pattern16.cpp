5edf5b2918708d733841e309d365c190fb11117a
Sri-Mayur <mayursrivastava877@gmail.com>
2023-09-09 23:03:34 +0530

    Reverse Letter Triangle

---
+++ Pattern16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+void print(int n){
+    
+    for(int i=0;i<n;i++){
+        char ch= 'A'+i;
+        for(int j=0; j<=i;j++){
+            cout<<ch<<" ";
+            
+        }
+        
+        cout<<endl;
+    }}
+
+int main() {
+    int n,t;
+    cout<<"Enter the no. of Test Cases:";
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        cout<<"Enter the no. of rows in TestCase"<<i+1<<":";
+        cin>>n;
+        print(n);
+    }}
