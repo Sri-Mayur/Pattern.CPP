43b85e6e3b3b058e0416962f11571a9d2ac96304
Sri-Mayur <mayursrivastava877@gmail.com>
2023-09-09 23:02:36 +0530

    Increasing Letter Traingle

---
+++ Pattern14.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+void print(int n){
+    
+    for(int i=0;i<n;i++){
+        char ch= 'A';
+        for(int j=0; j<=i;j++){
+            cout<<ch++<<" ";
+            
+        }
+        cout<<endl;
+    }}
+
+int main() {
+    int n,t;
+    cout<<"Enter the no. of Test Cases:";
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        cout<<"Enter the no. of rows in TestCase"<<i+1<<":";
+        cin>>n;
+        print(n);
+    }}
